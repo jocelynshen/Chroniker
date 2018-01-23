244b682f6471daa302261afd1bd0a864530bc12b
Jeffrey Shen <jshen@andover.edu>
2018-01-23 11:11:03 -0500

    added facebook scrape file and documentation

---
+++ facebook_skrape.py
@@ -0,0 +1,38 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+"""
+This facebook_skrape.py script...
+
+Attributes:
+    module_level_variable1 (int): [description]
+
+Todo:
+    * For module TODOs
+"""
+
+module_level_variable1 = 12345
+
+def function_with_types_in_docstring(param1, param2):
+    """
+
+    Args:
+        param1 (int): The first parameter.
+        param2 (str): The second parameter.
+
+    Returns:
+        bool: The return value. True for success, False otherwise.
+    """
+
+
+    """
